40542d1702848f12d2dfd17b184b13eb2d486f35
Lucas Gomes <lgomeszx@gmail.com>
2023-04-06 12:56:00 -0300

    Desafio 02

---
+++ NavegationModal02/NavegationModal02/Tela01/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NavegationModal02
+//
+//  Created by Lucas Gomesx on 06/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tappedButton(_ sender: Any) {
+        let vc: Tela02ViewController? = UIStoryboard(name: "Tela02ViewController", bundle: nil).instantiateViewController(withIdentifier: "Tela02ViewController") as? Tela02ViewController
+        vc?.modalPresentationStyle = .fullScreen
+        present(vc ?? UIViewController(), animated: true)
+    }
+    
+}
+
